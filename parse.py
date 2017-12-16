a8d00b238de61995165eebb75fc023bec7c2a851
gracecuenca <gcuenca@stuy.edu>
2017-12-16 17:44:36 -0500

    comments to clarify

--- parse.py
+++ parse.py
@@ -3,21 +3,22 @@ from pprint import pprint
 
 data = json.load(open('formatted_output.json'))
 
-#pprint(data['entity'][50])
-#pprint(data['entity'][2])
-#pprint(data['entity'][3])
+#data['entity'] contains all the train time information
 d = data['entity']
-#pprint(str(d[0]['trip_update']['stop_time_update'][2]))
 
+#parsing through the entities
+#increment by 2 because the odd ones don't have time data
 for i in range(0, len(d), 2):
-    #print(label = d[i]['trip'])
+    #attempting to only grab 1 train data
     label = ""
     try:
         label = d[i]['trip_update']['trip']['route_id']
     except:
         pass
+    #change label to whatever train you want
     if label == "1":
         print("\n========================================================")
+        #don't really know what train id is but yeah
         print("train id: " + str(d[i]['id']))
         print("========================================================")
         try:
