2876fd29155aa04d8518d2060b9485bdeb1d6af6
Joyce Wu <jwu26@stuy.edu>
2017-12-16 21:29:04 -0500

    some beautifully formatted csv files :fire:

--- parse.py
+++ parse.py
@@ -6,7 +6,7 @@ data = json.load(open('formatted_output.json'))
 #data['entity'] contains all the train time information
 d = data['entity']
 
-heading = "line,stop_id,train_id,arrival time"
+heading = "line,stop_id,arrival time"
 
 #parsing through the entities
 #increment by 2 because the odd ones don't have time data
@@ -14,29 +14,24 @@ for i in range(0, len(d), 2):
     with open('output.csv','a') as f:
         #attempting to only grab 1 train data
         line = "" #next line of the csv file
-        label = ""
+        label = "1" #train line that we want
         try:
-            label = d[i]['trip_update']['trip']['route_id']
-        except:
-            pass
-        #change label to whatever train you want
-        if label == "1":
-            line = line + label+","
-            print("\n========================================================")
-            #don't really know what train id is but yeah
-            print("train id: " + str(d[i]['id']))
-            line = line + str(d[i]['id']) + ","
-            print("========================================================")
-            try:
+            train_line = d[i]['trip_update']['trip']['route_id']
+            if train_line == label:
+                print("\n========================================================")
                 ent = d[i]['trip_update']['stop_time_update']
                 for x in range(0, len(ent)):
+                    #train_line
+                    line = label + ","
+                    #stop_id
                     print("\nstop id: " + str(ent[x]['stop_id']))
                     line = line + str(ent[x]['stop_id'])+","
-                    print("unix arrival time: " + str(ent[x]['arrival']['time']) + ",")
-                    line = line + str(ent[x]['arrival']['time'])+","
+                    #arrival_time
+                    print("unix arrival time: " + str(ent[x]['arrival']['time']))
+                    line = line + str(ent[x]['arrival']['time'])
                     #print("\n" + str(d[i]['trip_update']['stop_time_update'][0]['arrival']['time']) + ",")
-                    #print(line)
-                f.write(line+"\n")
-            except:
-                pass
+                    print(line)
+                    f.write(line+"\n")
+        except:
+            pass
     f.close()
