babc1a2d1ba3e40a2f3e4b136233397b91a4fbf9
gracecuenca <gcuenca@stuy.edu>
2017-12-18 11:08:52 -0500

    created stop dictionary

---
+++ app.py
@@ -0,0 +1,12 @@
+stops = {
+    "142": "South Ferry",
+    "137": "Chambers Street",
+    "127": "42 Street Times Square",
+    "125": "59 Street Columbus Circle",
+    "120": "96 Street",
+    "115": "137 Street",
+    "112": "168 Street",
+    "104": "231 Street",
+    "103": "238 Street",
+    "101": "242 Street"
+}
