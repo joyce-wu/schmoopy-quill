3ee7c0d6261759e3c8cb711a96c3d53cb75f8b95
gracecuenca <gcuenca@stuy.edu>
2017-12-18 22:20:19 -0500

    oh no

--- app.py
+++ app.py
@@ -2,34 +2,59 @@ import numpy as np
 import pandas as pd
 
 def csv_to_array(file):
+    i=0
+    new = []
+    ret = []
     f = open(file, 'r')
     data = f.read()
-    lines = data.split("\n")
-    ret = []
-    for line in lines:
-        array = line.split(",")
-        ret.append(array)
+    sections = data.split("\n\n")
+    counter = len(sections)
+    while i < counter:
+        lines = sections[i].split("\n")
+        for line in lines:
+            array = line.split(",")
+            new.append(array)
+        ret.append(new)
+        new = []
+        i+=1
     return ret
 
-def create_time_table(file):
-    row = 25
+def create_time_table_sat(file):
+    data = csv_to_array(file)
+    group = len(data)
+    row = len(data[0])
     column = 10
     new_row = []
+    z = 0
+    x = 0
+    prev_row = 0
     table = [[0 for x in range(column)] for y in range(row)]
-    data = csv_to_array(file)
-    for i in range(row):
-        for y in range(column):
-            table[i][y] = data[i][y]
-    for i in range(9, 32):
-        for y in range(column):
-            new_row.append(table[i][y]+str(8*60))
-        table.append(new_row)
-        new_row = []
+    while z < group:
+        section = data[z]
+        row = len(data[z])
+        if z == 0:
+            for i in range(row):
+                for y in range(column):
+                    table[i][y] = section[i][y]
+        else:
+            for i in range(prev_row-1 , prev_row + row):
+                for y in range(column):
+                    table[i][y] = section[i][y]
+        if x < 3:
+            end =  int((int(data[z+1][0][0])- int(section[len(section) -1][0]))/(60*8))
+            for i in range(row-1, end):
+                for y in range(column):
+                    new_row.append(table[i][y]+str(8*60))
+                table.append(new_row)
+                new_row = []
+            prev_row = prev_row + i
+            print(len(table[0]))
+            print(len(table))
+        z+=1
+        x+=1
     print(table)
 
-
-create_time_table('sat_timetable_s.csv')
-
+create_time_table_sat('sat_timetable_n.csv')
 
 stops = {
     "142": "South Ferry",
