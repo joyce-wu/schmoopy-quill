5e59aa2253b824fb5d86da8d54d9b78e4266a50c
gracecuenca <gcuenca@stuy.edu>
2017-12-19 19:05:48 -0500

    started jupyter

---
+++ graph.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
