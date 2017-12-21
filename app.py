905b72327e232e95164f52a0739cc78d6a864183
Joyce Wu <jwu26@stuy.edu>
2017-12-20 22:17:19 -0500

    :fire: :fire: 2Sigma fun times :fire: :fire:

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import numpy as np
 import pandas as pd
 import copy
 import csv
+import random
+
+stops = { #corresponding column to stop
+    "142": 0,
+    "137": 1,
+    "127": 2,
+    "125": 3,
+    "120": 4,
+    "115": 5,
+    "112": 6,
+    "104": 7,
+    "103": 8,
+    "101": 9
+}
 
 def csv_to_array(file):
     i=0
@@ -20,6 +34,63 @@ def csv_to_array(file):
         new = []
         i+=1
     return ret
+
+def create_actual_table(actual, day):
+    f = open(actual, 'r')
+    data = f.read()
+    lines = data.split("\n")
+    ret = []
+    for line in lines:
+        if(line == ""):
+            pass
+        else:
+            stop = line.split(",")[1]
+            time = line.split(",")[2]
+            closest_time = find_closest_time(stop, time, day)
+            #print(closest_time)
+            if(closest_time != None):
+                print("closest: " + str(closest_time) + "time: " + time)
+                time_difference = int(time) - closest_time
+                #print(time_difference)
+                ret.append([int(stop[0:3]), time_difference])
+    return ret
+
+def find_closest_time(stop, time, day):
+    direction = stop[-1]
+    time = int(time)
+    if(day == "monday" or day == "tuesday"):
+        file_name = "weekday/scheduled/weekday_" + direction + ".csv"
+    elif(day == "saturday"):
+        file_name = "saturday/scheduled/sat_" + direction + ".csv"
+    scheduled_array = csv_to_array(file_name)
+    #print(scheduled_array)
+    try:
+        stop = stops[stop[0:3]]
+        #print(stop)
+        smallest_diff = time - int(scheduled_array[0][0][int(stop)])
+        smallest_time = int(scheduled_array[0][0][int(stop)])
+        #print(smallest_time)
+        for row in range(len(scheduled_array)):
+            if time - int(scheduled_array[0][row][stop]) < smallest_diff:
+                print(smallest_diff)
+                smallest_time = int(scheduled_array[0][row][stop])
+        return smallest_time
+    except:
+        return
+
+def unix_to_min(array):
+    for i in range(len(array)):
+        array[i][1] = 60 - array[i][1]/3600
+    return array
+
+
+ret = create_actual_table("weekday/monday/actual/monday.csv", "monday")
+print(ret)
+la = unix_to_min(ret)
+print(ret)
+
+
+
 
 def create_time_table_sat(file):
     data = csv_to_array(file)
@@ -78,17 +149,15 @@ def array_to_csv(array, file):
         f.write("\n")
     f.close()
 
-array_to_csv(create_time_table_sat('saturday/scheduled/sat_timetable_s.csv'), 'sat_s.csv')
+stop_ids = ['142', '137', '127', '125', '120', '115', '112', '104', '103', '101']
+def generate_times(stop_list, file):
+    i = 0
+    while i < 40:
+        print random.uniform(0.0, 8.0)
+        i += 1
 
-stops = {
-    "142": "South Ferry",
-    "137": "Chambers Street",
-    "127": "42 Street Times Square",
-    "125": "59 Street Columbus Circle",
-    "120": "96 Street",
-    "115": "137 Street",
-    "112": "168 Street",
-    "104": "231 Street",
-    "103": "238 Street",
-    "101": "242 Street"
-}
+generate_times(stop_ids, 'time_dif.csv')
+
+
+
+#array_to_csv(create_time_table_sat('saturday/scheduled/sat_timetable_s.csv'), 'sat_s.csv')
