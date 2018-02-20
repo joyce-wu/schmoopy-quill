330734805c419f4f549faf6fe4d18753e8af5649
gracecuenca <gcuenca@stuy.edu>
2018-02-19 21:04:21 -0500

    testing: check tmrw

---
+++ makefile
@@ -0,0 +1,12 @@
+pull: pull.py
+	python pull.py
+
+clean:
+	rm 1.json
+	rm 26.json
+	rm 16.json
+	rm 21.json
+	rm 2.json
+	rm 31.json
+	rm 36.json
+	rm 51.json
--- pull.py
+++ pull.py
@@ -1,10 +1,12 @@
-#import schedule
+import schedule
 import time
 import urllib
 import os
+import subprocess
+
+#export PYTHONPATH="${PYTHONPATH}/usr/local/lib/python2.7/site-packages:/usr/lib/python2.7/site-packages"
 
 key = "233db5c5454f7a125d8e129aff2503d1"
-global count = 0
 
 urls = {
     #1,2,3,4,5,6,S
@@ -25,12 +27,20 @@ urls = {
     "51": " http://datamine.mta.info/mta_esi.php?key=" + key + "&feed_id=51"
 }
 
+#updates at 11:59 every night to respective .json file
 def convert():
-    os.system("gtfs_realtime_json/dist/gtfs_realtime_json " + urls["1"] > "output"+count+".json")
-    count+=1
+    #os.system("gtfs_realtime_json/dist/gtfs_realtime_json " +str(urls["1"])+ " > 1.json")
+    os.system("gtfs_realtime_json/dist/gtfs_realtime_json 'http://datamine.mta.info/mta_esi.php?key=233db5c5454f7a125d8e129aff2503d1&feed_id=1' > 1.json")
+    os.system("gtfs_realtime_json/dist/gtfs_realtime_json 'http://datamine.mta.info/mta_esi.php?key=233db5c5454f7a125d8e129aff2503d1&feed_id=26' > 26.json")
+    os.system("gtfs_realtime_json/dist/gtfs_realtime_json 'http://datamine.mta.info/mta_esi.php?key=233db5c5454f7a125d8e129aff2503d1&feed_id=16' > 16.json")
+    os.system("gtfs_realtime_json/dist/gtfs_realtime_json 'http://datamine.mta.info/mta_esi.php?key=233db5c5454f7a125d8e129aff2503d1&feed_id=21' > 21.json")
+    os.system("gtfs_realtime_json/dist/gtfs_realtime_json 'http://datamine.mta.info/mta_esi.php?key=233db5c5454f7a125d8e129aff2503d1&feed_id=2' > 2.json")
+    os.system("gtfs_realtime_json/dist/gtfs_realtime_json 'http://datamine.mta.info/mta_esi.php?key=233db5c5454f7a125d8e129aff2503d1&feed_id=31' > 31.json")
+    os.system("gtfs_realtime_json/dist/gtfs_realtime_json 'http://datamine.mta.info/mta_esi.php?key=233db5c5454f7a125d8e129aff2503d1&feed_id=36' > 36.json")
+    os.system("gtfs_realtime_json/dist/gtfs_realtime_json 'http://datamine.mta.info/mta_esi.php?key=233db5c5454f7a125d8e129aff2503d1&feed_id=51' > 51.json")
 
 def main():
-    convert()
+    schedule.every().day.at("23:59").do(convert)
 
 if __name__ == '__main__':
     main()
